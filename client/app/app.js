71df0d5af8742ae22f077a3db14d68b1db194023
kevin <kevinmcfung@gmail.com>
2017-01-16 16:02:53 -0500

    switched calorie tracking from scope variable (immobile) to a factory

--- client/app/app.js
+++ client/app/app.js
@@ -1,19 +1,36 @@
-console.log("app.js loaded");
+
+var DEBUG = true;
+if (DEBUG) console.log("app.js loaded");
+// if (DEBUG) console.log("");
+
 
 angular.module('iifyMVP', [])
-  .controller('CalorieController', function($scope) {
-    console.log("app.js: CalorieController loaded");
-    console.log($scope.calories.totalCals);
-
-
-    var macroSplit = function() {
-      console.log('calories submitted');
+  .factory('calorieTracker', function() {
+    var userCals = 0;
+    var setCals = function(cals) {
+      userCals = cals;
+      return userCals;
     }
-    var totalCals = $scope.calories.totalCals;
+    var getCals = function() {
+      return userCals;
+    }
 
+    return {
+      setCals: setCals,
+      getCals: getCals
+    }
+  })
+  .controller('CalorieController', function($scope, calorieTracker) {
+    var calories = this;
 
+    if (DEBUG) console.log("app.js: CalorieController loaded");
+    if (DEBUG) console.log("$scope: ", $scope);
 
-
-
+    calories.macroSplit = function() {
+      calorieTracker.setCals($scope.totalCals);
+      if (DEBUG) console.log(calorieTracker.getCals(), 'calories submitted');
+    };
+  })
+  .controller('MacroController', function($scope, calorieTracker) {
 
   });
